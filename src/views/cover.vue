29823f07e6f83e922fc146327dca1163b26319e8
syzygy608 <hankwu99@gmail.com>
2026-01-14 13:21:32 +0800

    use react and bun to build new version website

--- src/views/cover.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="relative min-h-[calc(100dvh-3rem)] w-full">
-        <div class="bg-[url('@/assets/background.jpg')] bg-cover bg-center absolute inset-0 z-0"></div>
-        <div class="absolute inset-0 z-5 bg-white opacity-30"></div>
-    </div>
-</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  publicDir: 'public',
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@fortawesome/fontawesome-free/css/all.css";`
-      }
-    }
-  }
+  plugins: [react()],
 })
